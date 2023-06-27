a8b1534bad47673df91a53318182c3250e3354aa
demaceo <hdemaceo@gmail.com>
2023-06-26 18:48:31 -0600

    import Route and Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 // import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups.js";
+import NewMeetupPage from "./pages/NewMeetup.js";
+import FavoritesPage from "./pages/Favorites.js";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
     </div>
   );
 }
