c005fad52bfa5d37fe19c234af7a88e94a6c77b8
demaceo <hdemaceo@gmail.com>
2023-06-27 15:50:47 -0600

    fetch stored meetup data

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,31 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./AllMeetups.css";
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_DATA = [
-  {
-    id: "m1",
-    title: "This is a first meetup",
-    image: `https://picsum.photos/1000? ${+Math.random()}`,
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is an amazing meetup spot which you definitely should not miss. It will be a lot of fun!",
-  },
-  {
-    id: "m2",
-    title: "This is a second meetup",
-    image: `https://picsum.photos/1000? ${+Math.random()}`,
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is an amazing meetup spot which you definitely should not miss. It will be a lot of fun!",
-  },
-];
+// const DUMMY_DATA = [
+//   {
+//     id: "m1",
+//     title: "This is a first meetup",
+//     image: `https://picsum.photos/1000? ${+Math.random()}`,
+//     address: "Meetupstreet 5, 12345 Meetup City",
+//     description:
+//       "This is an amazing meetup spot which you definitely should not miss. It will be a lot of fun!",
+//   },
+//   {
+//     id: "m2",
+//     title: "This is a second meetup",
+//     image: `https://picsum.photos/1000? ${+Math.random()}`,
+//     address: "Meetupstreet 5, 12345 Meetup City",
+//     description:
+//       "This is an amazing meetup spot which you definitely should not miss. It will be a lot of fun!",
+//   },
+// ];
 
 function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://react-getting-started-c2bb9-default-rtdb.firebaseio.com/meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+          meetups.push(meetup);
+        }
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
   return (
     <section>
       <h1>All Meetups Page</h1>
-      <MeetupList meetups={DUMMY_DATA} />
+      <MeetupList meetups={loadedMeetups} />
     </section>
   );
 }
