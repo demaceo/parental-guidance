1757c70dd51236eb5006f1fea62dbed9b3a3748f
demaceo <hdemaceo@gmail.com>
2023-06-27 14:58:02 -0600

    create Layout component

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Layout.css";
+import MainNavigation from "./MainNavigation";
+
+function Layout(props) {
+  return (
+    <div>
+      <MainNavigation />
+      <main className='main'>{props.children}</main>
+    </div>
+  );
+}
+
+export default Layout;
