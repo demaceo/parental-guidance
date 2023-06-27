4a40a88374df2022e2fdd760c776e214ec543628
demaceo <hdemaceo@gmail.com>
2023-06-27 11:40:39 -0600

    import MainNavigation component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import { Routes, Route } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups.js";
 import NewMeetupPage from "./pages/NewMeetup.js";
 import FavoritesPage from "./pages/Favorites.js";
+import MainNavigation from "./components/layout/MainNavigation.js";
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
         <Route path="/new-meetup" element={<NewMeetupPage />} />
