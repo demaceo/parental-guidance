1d652941f660f0719407833c60635da1a09e5763
demaceo <hdemaceo@gmail.com>
2023-06-28 15:15:34 -0600

    remove dummy data

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -2,25 +2,6 @@ import React, { useState, useEffect } from "react";
 import "./AllMeetups.css";
 import MeetupList from "../components/meetups/MeetupList";
 
-// const DUMMY_DATA = [
-//   {
-//     id: "m1",
-//     title: "This is a first meetup",
-//     image: `https://picsum.photos/1000? ${+Math.random()}`,
-//     address: "Meetupstreet 5, 12345 Meetup City",
-//     description:
-//       "This is an amazing meetup spot which you definitely should not miss. It will be a lot of fun!",
-//   },
-//   {
-//     id: "m2",
-//     title: "This is a second meetup",
-//     image: `https://picsum.photos/1000? ${+Math.random()}`,
-//     address: "Meetupstreet 5, 12345 Meetup City",
-//     description:
-//       "This is an amazing meetup spot which you definitely should not miss. It will be a lot of fun!",
-//   },
-// ];
-
 function AllMeetupsPage() {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedMeetups, setLoadedMeetups] = useState([]);
